9266f99a90ca6e23367600201d3f9d08bac69eba
JohanurRahman <johanur.rahman@gmail.com>
2018-12-19 20:30:03 +0600

    Added formatted date input

--- src/app/material-custom-datepicker/material-custom-datepicker.component.ts
+++ src/app/material-custom-datepicker/material-custom-datepicker.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DateAdapter, MAT_DATE_FORMATS, } from '@angular/material/core';
 import { AppDateAdapter, APP_DATE_FORMATS } from './date.adapter';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import * as _moment from 'moment';
 
 @Component({
   selector: 'app-material-custom-datepicker',
@@ -18,9 +20,24 @@ import { AppDateAdapter, APP_DATE_FORMATS } from './date.adapter';
 export class MaterialCustomDatepickerComponent implements OnInit {
 
 
-  constructor() { }
+  formData: FormGroup;
 
-  ngOnInit() { }
+  initialDate;
+  formattedDate1;
+  formattedDate2;
 
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+      this.formData = this._formBuilder.group({
+        date: ['']
+      })
+   }
+
+   onChange(event){
+     this.initialDate = event.value;
+     this.formattedDate1 = _moment(event.value).format('YYYY-MM-DD');
+     this.formattedDate2 = _moment(event.value).format('DD-MMM-YYYY');
+   }
 
 }
